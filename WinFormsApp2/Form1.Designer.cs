422c24d5a5c64a8b8da59833fb5c0912aa30c521
WooYoung <77717717+rhkrdndud22@users.noreply.github.com>
2021-09-02 21:39:31 +0900

    baseball

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.nBtn1 = new System.Windows.Forms.Button();
+            this.nBtn2 = new System.Windows.Forms.Button();
+            this.nBtn3 = new System.Windows.Forms.Button();
+            this.nBtn4 = new System.Windows.Forms.Button();
+            this.nBtn5 = new System.Windows.Forms.Button();
+            this.nBtn6 = new System.Windows.Forms.Button();
+            this.nBtn7 = new System.Windows.Forms.Button();
+            this.nBtn8 = new System.Windows.Forms.Button();
+            this.cancelB = new System.Windows.Forms.Button();
+            this.subB = new System.Windows.Forms.Button();
+            this.answerB = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nBtn1
+            // 
+            this.nBtn1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nBtn1.BackgroundImage = global::WinFormsApp2.Properties.Resources._base;
+            this.nBtn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.nBtn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nBtn1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.nBtn1.FlatAppearance.BorderSize = 0;
+            this.nBtn1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.nBtn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.nBtn1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nBtn1.Location = new System.Drawing.Point(31, 124);
+            this.nBtn1.Name = "nBtn1";
+            this.nBtn1.Size = new System.Drawing.Size(48, 39);
+            this.nBtn1.TabIndex = 0;
+            this.nBtn1.Text = "1";
+            this.nBtn1.UseVisualStyleBackColor = false;
+            this.nBtn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nBtn2
+            // 
+            this.nBtn2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nBtn2.BackgroundImage = global::WinFormsApp2.Properties.Resources._base;
+            this.nBtn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.nBtn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nBtn2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.nBtn2.FlatAppearance.BorderSize = 0;
+            this.nBtn2.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.nBtn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.nBtn2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nBtn2.Location = new System.Drawing.Point(119, 124);
+            this.nBtn2.Name = "nBtn2";
+            this.nBtn2.Size = new System.Drawing.Size(48, 39);
+            this.nBtn2.TabIndex = 1;
+            this.nBtn2.Text = "2";
+            this.nBtn2.UseVisualStyleBackColor = false;
+            this.nBtn2.Click += new System.EventHandler(this.nBtn2_Click);
+            // 
+            // nBtn3
+            // 
+            this.nBtn3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nBtn3.BackgroundImage = global::WinFormsApp2.Properties.Resources._base;
+            this.nBtn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.nBtn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nBtn3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.nBtn3.FlatAppearance.BorderSize = 0;
+            this.nBtn3.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.nBtn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.nBtn3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nBtn3.Location = new System.Drawing.Point(213, 124);
+            this.nBtn3.Name = "nBtn3";
+            this.nBtn3.Size = new System.Drawing.Size(48, 39);
+            this.nBtn3.TabIndex = 2;
+            this.nBtn3.Text = "3";
+            this.nBtn3.UseVisualStyleBackColor = false;
+            this.nBtn3.Click += new System.EventHandler(this.nBtn3_Click);
+            // 
+            // nBtn4
+            // 
+            this.nBtn4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nBtn4.BackgroundImage = global::WinFormsApp2.Properties.Resources._base;
+            this.nBtn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.nBtn4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nBtn4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.nBtn4.FlatAppearance.BorderSize = 0;
+            this.nBtn4.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.nBtn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.nBtn4.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nBtn4.Location = new System.Drawing.Point(307, 124);
+            this.nBtn4.Name = "nBtn4";
+            this.nBtn4.Size = new System.Drawing.Size(48, 39);
+            this.nBtn4.TabIndex = 3;
+            this.nBtn4.Text = "4";
+            this.nBtn4.UseVisualStyleBackColor = false;
+            this.nBtn4.Click += new System.EventHandler(this.nBtn4_Click);
+            // 
+            // nBtn5
+            // 
+            this.nBtn5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nBtn5.BackgroundImage = global::WinFormsApp2.Properties.Resources._base;
+            this.nBtn5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.nBtn5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nBtn5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.nBtn5.FlatAppearance.BorderSize = 0;
+            this.nBtn5.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.nBtn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.nBtn5.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nBtn5.Location = new System.Drawing.Point(31, 173);
+            this.nBtn5.Name = "nBtn5";
+            this.nBtn5.Size = new System.Drawing.Size(48, 39);
+            this.nBtn5.TabIndex = 4;
+            this.nBtn5.Text = "5";
+            this.nBtn5.UseVisualStyleBackColor = false;
+            this.nBtn5.Click += new System.EventHandler(this.nBtn5_Click);
+            // 
+            // nBtn6
+            // 
+            this.nBtn6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nBtn6.BackgroundImage = global::WinFormsApp2.Properties.Resources._base;
+            this.nBtn6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.nBtn6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nBtn6.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.nBtn6.FlatAppearance.BorderSize = 0;
+            this.nBtn6.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.nBtn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.nBtn6.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nBtn6.Location = new System.Drawing.Point(119, 173);
+            this.nBtn6.Name = "nBtn6";
+            this.nBtn6.Size = new System.Drawing.Size(48, 39);
+            this.nBtn6.TabIndex = 5;
+            this.nBtn6.Text = "6";
+            this.nBtn6.UseVisualStyleBackColor = false;
+            this.nBtn6.Click += new System.EventHandler(this.nBtn6_Click);
+            // 
+            // nBtn7
+            // 
+            this.nBtn7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nBtn7.BackgroundImage = global::WinFormsApp2.Properties.Resources._base;
+            this.nBtn7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.nBtn7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nBtn7.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.nBtn7.FlatAppearance.BorderSize = 0;
+            this.nBtn7.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.nBtn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.nBtn7.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nBtn7.Location = new System.Drawing.Point(213, 173);
+            this.nBtn7.Name = "nBtn7";
+            this.nBtn7.Size = new System.Drawing.Size(48, 39);
+            this.nBtn7.TabIndex = 6;
+            this.nBtn7.Text = "7";
+            this.nBtn7.UseVisualStyleBackColor = false;
+            this.nBtn7.Click += new System.EventHandler(this.nBtn7_Click);
+            // 
+            // nBtn8
+            // 
+            this.nBtn8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nBtn8.BackgroundImage = global::WinFormsApp2.Properties.Resources._base;
+            this.nBtn8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.nBtn8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nBtn8.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.nBtn8.FlatAppearance.BorderSize = 0;
+            this.nBtn8.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.nBtn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.nBtn8.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nBtn8.Location = new System.Drawing.Point(307, 173);
+            this.nBtn8.Name = "nBtn8";
+            this.nBtn8.Size = new System.Drawing.Size(48, 39);
+            this.nBtn8.TabIndex = 7;
+            this.nBtn8.Text = "8";
+            this.nBtn8.UseVisualStyleBackColor = false;
+            this.nBtn8.Click += new System.EventHandler(this.nBtn8_Click);
+            // 
+            // cancelB
+            // 
+            this.cancelB.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.cancelB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cancelB.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.cancelB.FlatAppearance.BorderSize = 10;
+            this.cancelB.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.cancelB.Location = new System.Drawing.Point(32, 231);
+            this.cancelB.Name = "cancelB";
+            this.cancelB.Size = new System.Drawing.Size(107, 39);
+            this.cancelB.TabIndex = 8;
+            this.cancelB.Text = "취소";
+            this.cancelB.UseVisualStyleBackColor = false;
+            this.cancelB.Click += new System.EventHandler(this.cancelB_Click);
+            // 
+            // subB
+            // 
+            this.subB.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.subB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.subB.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.subB.FlatAppearance.BorderSize = 10;
+            this.subB.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.subB.Location = new System.Drawing.Point(167, 231);
+            this.subB.Name = "subB";
+            this.subB.Size = new System.Drawing.Size(107, 39);
+            this.subB.TabIndex = 9;
+            this.subB.Text = "제출";
+            this.subB.UseVisualStyleBackColor = false;
+            this.subB.Click += new System.EventHandler(this.subB_Click);
+            // 
+            // answerB
+            // 
+            this.answerB.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.answerB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.answerB.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.answerB.FlatAppearance.BorderSize = 10;
+            this.answerB.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.answerB.Location = new System.Drawing.Point(32, 304);
+            this.answerB.Name = "answerB";
+            this.answerB.Size = new System.Drawing.Size(107, 39);
+            this.answerB.TabIndex = 10;
+            this.answerB.Text = "정답은";
+            this.answerB.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::WinFormsApp2.Properties.Resources.adf;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.nBtn7);
+            this.panel1.Controls.Add(this.answerB);
+            this.panel1.Controls.Add(this.cancelB);
+            this.panel1.Controls.Add(this.subB);
+            this.panel1.Controls.Add(this.nBtn1);
+            this.panel1.Controls.Add(this.nBtn2);
+            this.panel1.Controls.Add(this.nBtn3);
+            this.panel1.Controls.Add(this.nBtn4);
+            this.panel1.Controls.Add(this.nBtn5);
+            this.panel1.Controls.Add(this.nBtn8);
+            this.panel1.Controls.Add(this.nBtn6);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 411);
+            this.panel1.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("문체부 제목 돋음체", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(307, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 78);
+            this.label1.TabIndex = 14;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 411);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nBtn1;
+        private System.Windows.Forms.Button nBtn2;
+        private System.Windows.Forms.Button nBtn3;
+        private System.Windows.Forms.Button nBtn4;
+        private System.Windows.Forms.Button nBtn5;
+        private System.Windows.Forms.Button nBtn6;
+        private System.Windows.Forms.Button nBtn7;
+        private System.Windows.Forms.Button nBtn8;
+        private System.Windows.Forms.Button cancelB;
+        private System.Windows.Forms.Button subB;
+        private System.Windows.Forms.Button answerB;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(count_down);
+            timer1.Interval = 150;
+            timer1.Start();
+            textBox1.MaxLength = 3;
+        }
+
+        int duration = 0;
+
+        private void count_down(object sender, EventArgs e)
+        {
+
+            if (duration == 2000)
+            {
+                timer1.Stop();
+
+            }
+            else
+            {
+                duration++;
+                label1.Text = duration.ToString();
+            }
+        }
+        private bool _isProcessing;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+            nBtn1.Enabled = false;
+        }
+        
+
+        private void subB_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Stop();
+        }
+
+        private void nBtn2_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Text += "2";
+            nBtn1.Enabled = false;
+        }
+        private void nBtn3_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text += "3";
+            nBtn3.Enabled = false;
+        }
+        private void nBtn4_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text += "4";
+            nBtn4.Enabled = false;
+        }
+        private void nBtn5_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text += "5";
+            nBtn5.Enabled = false;
+        }
+        private void nBtn6_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text += "6";
+            nBtn6.Enabled = false;
+        }
+        private void nBtn7_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text += "7";
+            nBtn7.Enabled = false;
+        }
+        private void nBtn8_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text += "8";
+            nBtn8.Enabled = false;
+        }
+
+        private void cancelB_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            nBtn1.Enabled = true;
+            nBtn2.Enabled = true;
+            nBtn3.Enabled = true;
+            nBtn4.Enabled = true;
+            nBtn5.Enabled = true;
+            nBtn6.Enabled = true;
+            nBtn7.Enabled = true;
+            nBtn8.Enabled = true;
+           
+
+        }
+
+        private void n(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
